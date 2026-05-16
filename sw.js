8e81a751cd23e6080ec909c03e5dfa52d6b98b96
MAYUR PATIL <mayurpa2004@gmail.com>
2026-05-16 17:23:08 +0530

    Refactor service worker for caching and fetch handling
    Updated service worker to improve caching strategy and manage assets.

--- sw.js
+++ sw.js
@@ -1,7 +1,44 @@
-const CACHE_NAME = 'tripkhata-v1';
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['./', './index.html'])));
+const CACHE_NAME = 'tripkhata-cache-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+
+// Install the Service Worker and cache the files
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
 });
-self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+
+// Serve cached content when offline
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Return the cached version if found, otherwise fetch from the network
+                return response || fetch(event.request);
+            })
+    );
+});
+
+// Update the cache if a new version is available
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
